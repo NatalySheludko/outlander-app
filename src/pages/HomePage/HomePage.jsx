b6c17eaf46918378027e62f11f52bfcb7e893bab
Nataliia Sheludko <sheludkonataliyavaleriivna@gmail.com>
2024-09-29 21:57:50 +0300

    create home page component

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,25 @@
+import { useNavigate } from "react-router-dom";
+import BtnComponent from "../../components/BtnComponent/BtnComponent";
+import css from "./HomePage.module.css";
+
+
+export default function HomePage() {
+  const navigate = useNavigate();
+
+  const handleViewNowClick = () => {
+    navigate("/catalog");
+  };
+
+  return (
+    <div className={css.container}>
+      <div className={css.wrap}>
+        <h1 className={css.title}>Campers of your dreams</h1>
+        <p className={css.info}>
+          You can find everything you want in our catalog
+        </p>
+        <BtnComponent onClick={handleViewNowClick}> View Now</BtnComponent>
+      </div>
+    </div>
+  );
+}
+
